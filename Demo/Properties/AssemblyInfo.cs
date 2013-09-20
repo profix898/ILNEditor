a28739cc87a4207e6233016d1fc418e377c3a0d8
profix898 <codedev@profix898.de>
2013-09-20 23:37:14 +0200

    - task: NuGet Release v0.2

--- Demo/Properties/AssemblyInfo.cs
+++ Demo/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Demo")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright ©  2013 Thilo Wawrzik")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
